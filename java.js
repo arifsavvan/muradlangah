59f79efc907425af2ccd11e038759e37d5df52a2
arifsavvan <arifrahimsavvan@gmail.com>
2021-11-09 01:19:26 -0800

    frist commit

---
+++ java.js
@@ -0,0 +1,3 @@
+function fun(){
+	alert("you will order the product through the information provided in the right side of footer. you can order through whatsapp number (03129710285) or email muradlangah@gmail.com Thank ")
+}
